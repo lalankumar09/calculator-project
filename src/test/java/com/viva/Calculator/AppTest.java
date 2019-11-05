e9a1f4866a63b9404dcb22c108761e282d11d391
lalankumar09 <49072263+lalankumar09@users.noreply.github.com>
2019-11-05 16:49:01 +0530

    updating test

--- src/test/java/com/viva/Calculator/AppTest.java
+++ src/test/java/com/viva/Calculator/AppTest.java
@@ -18,39 +18,47 @@ public class AppTest
 	@Test
 	public void addTrue() {
 		assertEquals(25, app.add(12, 13));
+		System.out.println("addTrue Executed");
 	}
 	@Test
 	public void addFail() {
 		assertNotEquals(25, app.add(15, 13));
+		System.out.println("addFail Executed");
 	
 	}
 	
 	@Test
 	public void subTrue() {
 		assertEquals(3, app.sub(16, 13));
+		System.out.println("subTrue Executed");
 	}
 	@Test
 	public void subFail() {
 		assertNotEquals(2, app.sub(16, 13));
+		System.out.println("subFail Executed");
 	}
 	
 	@Test
 	public void mulTrue() {
 		assertEquals(25, app.mul(5, 5));
+		System.out.println("mulTrue Executed");
 	}
 	@Test
 	public void mulFail() {
 		assertNotEquals(25, app.mul(5, 6));
+		System.out.println("mulFail Executed");
 	}
 	
 	
 	@Test
 	public void divTrue() {
 		assertEquals(1, app.div(12, 12));
+		System.out.println("divTrue Executed");
 	}
 	@Test
 	public void divFail() {
 		assertNotEquals(1, app.div(24, 12));
+		System.out.println("divFail Executed");
 	}
 	@Test(expected = ArithmeticException.class)
         public void testDivideByZero() {
